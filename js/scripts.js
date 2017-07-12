8f532e89caa43a55492c1a95eccfadf233e28444
Kaili Nishihira and Michael Woldemedihin <kaili@>
2017-07-12 14:29:57 -0700

    Check if the input begins with a consonant, it will move the consecutive consonants to the end of the word and add ay.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,66 @@
 $(document).ready(function() {
  $("#pig-latin").submit(function(event) {
    event.preventDefault();
-//debugger;
+debugger;
    var userInput = $("#english").val();
    var output = pigLatin(userInput);
     $("#result").text(output);
  });
 });
 //business logic
+
 var pigLatin = function(input){
+//  debugger;
   if (!isNaN(input)) {
     return input;
   } else {
-    if (input[0] === "a" || input[0] === "e" || input[0] === "o" || input[0] === "i" ||nput[0] === "u") {
+    if (input[0] === "a" || input[0] === "e" || input[0] === "o" || input[0] === "i" || input[0] === "u") {
       input += "way";
       return input;
+    }else if (input[0] !== "a") {
+      var splitedInput = input.split('');
+      for (var i = 0; i < splitedInput.length; i++) {
+        if (splitedInput[i] === "a") {
+          var spliced = splitedInput.splice(0,i);
+          return splitedInput.join('') + spliced.join('') +  "ay";
+        }
+      }
     }
   }
 
-  //if input is a number return the number
-  //if input is not a number
-    //1. if input begiins with vowel, we add "way" to the end
-        // eg. eat = eatway
-    //2.if input begins with one or more consonant, then move the first consequative consonants to the end and add "ay" to the end of the word.
-        // eg. tom = omtay
-        // eg. stick = ickstay
-      //3. if the word begins with "qu" move "qu" to the end and add "ay" at the end of the word
-        //eg. quick = ickquay
-      //4. if the word begins with "qu" or have "-qu", then move "qu" or "-qu" to the end and add "ay" at the end of the word
-        //eg. question = estionquay
-        //eg. squirrel = irrelsquay
+  //A. if input is a number return the number
+  //B. if input is not a number
+        //1. if input begiins with vowel, we add "way"            to the end
+            // eg. eat = eatway
+        //2.if input begins with one or more  consonant, then move the first consequative consonants to the end and add "ay" to the end of the word.
+            // eg. tom = omtay
+            // eg. stick = ickstay
+        //3. if the word begins with "qu" move "qu" to the end and add "ay" at the end of the word
+            //eg. quick = ickquay
+          //4. if the word begins with "qu" or have "-qu", then move "qu" or "-qu" to the end and add "ay" at the end of the word
+            //eg. question = estionquay
+            //eg. squirrel = irrelsquay
 //Note: treat "y" as a consonant
 
+        // var vowels = ["a", "e", "i", "o", "u"];
+        // if (vowels.includes(input[0])) {
+        //
+        // }
+        // var sliceConsonant = function(string){
+        //   for (var i = 0; i < string.length; i++) {
+        //     var sliced;
+        //     if (vowels.includes(string[i])) {
+        //       sliced = string.slice(0,i);
+        //       input += sliced;
+        //     }else if(vowels.includes(string[i]) && string[i-1] === "q" {
+        //       sliced = string.slice();
+        //     }
+        //   }
+        // }
+        // var sliceQu = function(string){
+        //   for (var i = 0; i < string.length; i++) {
+        //
+        //   }
+        // }
 
 }
