36fb68268476b34881791ebc10617afcadb78fac
Kaili Nishihira and Michael Woldemedihin <michael@>
2017-07-12 11:38:34 -0700

    Check if input is a number, it will return a number if it is true.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-
+ $("#pig-latin").submit(function(event) {
+   event.preventDefault();
+debugger;
+   var userInput = $("#english").val();
+   var output = pigLatin(userInput);
+    $("#result").append(output);
+ });
 });
+//business logic
+var pigLatin = function(input){
+  if (!isNaN(input)) {
+    return input;
+  }
+
+  //if input is a number return the number
+  //if input is not a number
+    //1. if input begiins with vowel, we add "way" to the end
+        // eg. eat = eatway
+    //2.if input begins with one or more consonant, then move the first consequative consonants to the end and add "ay" to the end of the word.
+        // eg. tom = omtay
+        // eg. stick = ickstay
+      //3. if the word begins with "qu" move "qu" to the end and add "ay" at the end of the word
+        //eg. quick = ickquay
+      //4. if the word begins with "qu" or have "-qu", then move "qu" or "-qu" to the end and add "ay" at the end of the word
+        //eg. question = estionquay
+        //eg. squirrel = irrelsquay
+//Note: treat "y" as a consonant
+
+
+}
