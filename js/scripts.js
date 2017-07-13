affbecea754c08c9189b733219c6fb1ca9f3bace
Kaili Nishihira and Michael Woldemedihin <kaili@>
2017-07-12 17:09:50 -0700

    Change vowel checking logic into a function, it will refactor the code.

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,32 @@ debugger;
  });
 });
 //business logic
+var beginsWithVowel = function(stringInput){
+  var vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.includes(stringInput[0])) {
+    return true;
+  }else {
+    return false;
+  }
+}
+var isVowel = function(element){
+  var vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.includes(element)) {
+    return true;
+  }else {
+    return false;
+  }
+}
 
 var pigLatin = function(input){
-//  debugger;
   if (!isNaN(input)) {
     return input;
   } else {
-    if (input[0] === "a" || input[0] === "e" || input[0] === "o" || input[0] === "i" || input[0] === "u") {
+    var splitedInput = input.split('');
+    if (beginsWithVowel(splitedInput)) {
       input += "way";
       return input;
-    } else if (input[0] !== "a" || input[0] !== "e" || input[0] !== "o" || input[0] !== "i" || input[0] !== "u") {
-        var splitedInput = input.split('');
+    } else if (!beginsWithVowel(splitedInput)) {
         if (input[0] === "q" && input[1] === "u") {
           var spliceQu = splitedInput.splice(0,2);
           return splitedInput.join('') + spliceQu.join('') + "ay";
@@ -27,14 +42,15 @@ var pigLatin = function(input){
         return splitedInput.join('') + splice_Qu.join('') + "ay";
       }else {
         for (var i = 0; i < splitedInput.length; i++) {
-          if (splitedInput[i] === "a" ||splitedInput[i] === "e" ||splitedInput[i] === "i" ||splitedInput[i] === "o" ||splitedInput[i] === "u") {
+          if (isVowel(splitedInput[i])) {
             var spliced = splitedInput.splice(0,i);
             return splitedInput.join('') + spliced.join('') +  "ay";
           }
-      }
+        }
       }
     }
   }
+}
 
   //A. if input is a number return the number
   //B. if input is not a number
@@ -49,26 +65,3 @@ var pigLatin = function(input){
             //eg. question = estionquay
             //eg. squirrel = irrelsquay
 //Note: treat "y" as a consonant
-
-        // var vowels = ["a", "e", "i", "o", "u"];
-        // if (vowels.includes(input[0])) {
-        //
-        // }
-        // var sliceConsonant = function(string){
-        //   for (var i = 0; i < string.length; i++) {
-        //     var sliced;
-        //     if (vowels.includes(string[i])) {
-        //       sliced = string.slice(0,i);
-        //       input += sliced;
-        //     }else if(vowels.includes(string[i]) && string[i-1] === "q" {
-        //       sliced = string.slice();
-        //     }
-        //   }
-        // }
-        // var sliceQu = function(string){
-        //   for (var i = 0; i < string.length; i++) {
-        //
-        //   }
-        // }
-
-}
